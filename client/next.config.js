8da3b6c48323644b27c2cd6e19dd57951bb7211f
jiwoo <snowdrop1202@gmail.com>
2024-08-05 17:35:40 +0900

    chore: webpack5 변경

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify      : true
+    webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
@@ -25,4 +30,3 @@ module.exports = {
         ];
     }
 };
-
